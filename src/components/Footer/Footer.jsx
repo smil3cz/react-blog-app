2a2e5366d9b01f5a05f65835f348ec43ed1c62ac
smil3cz <jan.melicherik@email.cz>
2021-02-08 12:32:19 +0100

    created footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>React Blogg App &#169; Jan Melicherík</p>
+      <p>Feel free to use it, don't claim it as your own!</p>
+    </footer>
+  );
+};
+export default Footer;
