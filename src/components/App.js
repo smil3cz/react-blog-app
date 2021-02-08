ec2b0b2a2e2b04dc8a5e48d06f63806925add542
smil3cz <jan.melicherik@email.cz>
2021-02-08 14:16:31 +0100

    login form created

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,31 @@ import "./App.scss";
 import Header from "./Header/Header";
 import HeaderAccount from "./HeaderAccount/HeaderAccount";
 import Footer from "./Footer/Footer";
+import LoginForm from "./LoginForm/LoginForm";
 import "./Header/styles.scss";
 import "./HeaderAccount/styles.scss";
 import "./Footer/styles.scss";
+import "./LoginForm/styles.scss";
 
 class App extends React.Component {
+  state = {
+    user: {
+      isLogged: false,
+      userName: "",
+      userPassword: "",
+      userApi: "",
+      tenantId: "",
+      createdAt: "",
+      lastUsedAt: null,
+      accessToken: "",
+    },
+  };
+
   render() {
     return (
       <Fragment>
         <Header headerAccount={<HeaderAccount />} />
+        <LoginForm />
         <Footer />
       </Fragment>
     );
