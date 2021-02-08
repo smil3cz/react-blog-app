242c564cb388087f74d39e81661752acf7c3cfaf
smil3cz <jan.melicherik@email.cz>
2021-02-08 12:13:21 +0100

    finished basic layout and styling of component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,34 @@
 const Header = ({ headerAccount }) => {
-  let userIsLogged = false;
+  let userIsLogged = true;
   return (
     <header className="header">
-      <div className="header__container">
-        <div className="header__menu">
-          <ul className="header__links">
-            <li className="header__link-item">
-              <a href="#" className="header__link">
-                Home
-              </a>
-            </li>
-            <li className="header__link-item">
-              <a href="#" className="header__link">
-                Recent articles
-              </a>
-            </li>
-            <li className="header__link-item">
-              <a href="#" className="header__link">
-                About
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="header__account">
-          {!userIsLogged ? (
-            <a href="#" className="header__login">
-              Log in<span>&rarr;</span>
+      <div className="header__menu">
+        <ul className="header__links">
+          <li className="header__link-item">
+            <a href="#" className="header__link">
+              Home
             </a>
-          ) : (
-            headerAccount
-          )}
-        </div>
+          </li>
+          <li className="header__link-item">
+            <a href="#" className="header__link">
+              Recent Articles
+            </a>
+          </li>
+          <li className="header__link-item">
+            <a href="#" className="header__link">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="header__account">
+        {!userIsLogged ? (
+          <a href="#" className="header__login">
+            Log in<span>&rarr;</span>
+          </a>
+        ) : (
+          headerAccount
+        )}
       </div>
     </header>
   );
