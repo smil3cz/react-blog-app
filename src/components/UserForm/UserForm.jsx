6559fb416a27404abf2d9954a85656f949756b03
smil3cz <jan.melicherik@email.cz>
2021-02-09 17:15:46 +0100

    fixed switching tabs in form

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,27 +1,49 @@
+let typeOfForm = "login";
+
+const changeFormType = () => {
+  const links = document.querySelectorAll(".form__type a");
+  links.forEach((link) =>
+    link.addEventListener("click", () => {
+      if (link.textContent.toLocaleLowerCase() === "login") {
+        typeOfForm = "login";
+        link.parentElement.classList.toggle("form__active");
+      } else if (link.textContent.toLocaleLowerCase() === "register") {
+        typeOfForm = "register";
+        link.parentElement.classList.toggle("form__active");
+      }
+    })
+  );
+};
+
 const UserForm = ({ isOpen }) => {
-  let typeOfForm = "login";
+  const LOGIN = "Log In";
+  const REGISTER = "Register";
+
   if (isOpen) {
     document.querySelector(".form").classList.add("form--open");
   }
+  changeFormType();
   return (
     <div className="form">
       <div className="form__type">
-        <div className="form__type-login">
+        <div className="form__active">
           <a href="#">Login</a>
         </div>
-        <div className="form__type-register">
+        <div>
           <a href="#">Register</a>
         </div>
       </div>
       <form className="form__container">
         <h3 className="form__heading">
-          {typeOfForm === "login" ? "Log In" : "Register"}
+          {typeOfForm === "login" ? LOGIN : REGISTER}
         </h3>
         <label htmlFor="userId">Username</label>
         <input className="username" id="userId" type="text" required />
         <label htmlFor="passId">Password</label>
         <input className="password" id="passId" type="password" required />
-        <button type="submit">Log In</button>
+        <button type="submit">
+          {typeOfForm === "login" ? LOGIN : REGISTER}
+        </button>
       </form>
     </div>
   );
