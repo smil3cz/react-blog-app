746d345e4dedd83c46331489efb09b1074f5f3ca
smil3cz <jan.melicherik@email.cz>
2021-02-16 23:03:06 +0100

    created loading spinner compoennt and insert into displayarticle before fetching articles

---
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner">
+      <div className="loading-spinner__spinner"></div>
+      <h3>Loading page</h3>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
